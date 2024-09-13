bc198905a2db34586ce8ac5a93d267d959c4ffde
Arshiya Mahmoodinezhad <159593495+X-arshiya-X@users.noreply.github.com>
2024-09-13 17:14:30 -0400

    "done"

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,18 @@
 public class Multiples {
+
+    public static int main() {
+        int count = 0;
+
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+        return count;
+    }
+
     public static int main(int n, int mult1, int mult2) {
 
         int count = 0;
